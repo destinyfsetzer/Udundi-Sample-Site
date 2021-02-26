57d882fa0547c489130356308c3b1069e1e082d6
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2021-02-25 18:44:58 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const toggleOnClick = () => {
+    let x = document.getElementById("more-details");
+    if (x.style.display ==="none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
