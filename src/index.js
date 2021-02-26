450dc4b53ac2602faa103895a54abe46812a87aa
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2021-02-26 14:52:50 -0600

    added js toggle functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-const toggleOnClick = () => {
-    let x = document.getElementById("more-details");
-    if (x.style.display ==="none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+const toggleClose = () => {
+  let close = document.getElementById("initial-view");
+  if (close.style.display === "none") {
+    close.style.display = "block";
+  } else {
+    close.style.display = "none";
+  }
+};
+
+const plusHandler = () => {
+  toggleClose();
+  toggleDetails();
+};
+
+const toggleDetails = () => {
+  let moreDetails = document.getElementById("more-details");
+  if (moreDetails.style.display === "block") {
+    moreDetails.style.display = "none";
+  } else {
+    moreDetails.style.display = "block";
+  }
+};
